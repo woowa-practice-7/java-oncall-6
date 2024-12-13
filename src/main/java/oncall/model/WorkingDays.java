f0ae689e4b41b8747fecb3fb1daea993e1d5083d
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-12-13 16:59:47 +0900

    feat(model): WorkingDay를 관리하는 일급컬렉션 WorkingDays 구현

---
+++ src/main/java/oncall/model/WorkingDays.java
@@ -0,0 +1,11 @@
+package oncall.model;
+
+import java.util.List;
+
+public class WorkingDays {
+    List<WorkingDay> workingDays;
+
+    public WorkingDays(List<WorkingDay> workingDays) {
+        this.workingDays = workingDays;
+    }
+}
