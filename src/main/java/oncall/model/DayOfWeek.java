baf98948345e8c39edfd33829e77436d52cfd739
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-12-13 17:00:42 +0900

    refactor(model) 요일 enum 수정

--- src/main/java/oncall/model/DayOfWeek.java
+++ src/main/java/oncall/model/DayOfWeek.java
@@ -1,26 +1,17 @@
 package oncall.model;
 
 public enum DayOfWeek {
-    MONDAY("월"),
-    TUESDAY("화"),
-    WEDNESDAY("수"),
-    THRUSDAY("목"),
-    FRIDAY("금"),
-    SATURDAY("토"),
-    SUNDAY("일");
+    월("월"),
+    화("화"),
+    수("수"),
+    목("목"),
+    금("금"),
+    토("토"),
+    일("일");
 
     private final String dayOfWeek;
 
     DayOfWeek(String dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
-    }
-
-    public static DayOfWeek getDayofWeek(String day) {
-        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
-            if (dayOfWeek.dayOfWeek.equals(day)) {
-                return dayOfWeek;
-            }
-        }
-        return null;
     }
 }
--- src/main/java/oncall/model/WorkingDay.java
+++ src/main/java/oncall/model/WorkingDay.java
@@ -7,18 +7,23 @@ public class WorkingDay {
     private Boolean isHoliday = false;
     private Worker worker = null;
 
-    public WorkingDay(Integer month, Integer day, String dayOfWeek) {
+    public WorkingDay(Integer month, Integer day, DayOfWeek dayOfWeek) {
         this.month = month;
         this.day = day;
-        this.dayOfWeek = DayOfWeek.getDayofWeek(dayOfWeek);
+        this.dayOfWeek = dayOfWeek;
         checkHoliday();
     }
 
     private void checkHoliday() {
+        if (dayOfWeek.equals(DayOfWeek.토) || dayOfWeek.equals(DayOfWeek.일)) {
+            isHoliday = true;
+            return;
+        }
+
         for (Holiday holiday : Holiday.values()) {
             if (holiday.getMonth().equals(month) && holiday.getDay().equals(day)) {
                 isHoliday = true;
-                break;
+                return;
             }
         }
     }
